3fe680c981b571b4b592723990b72079b24a6c01
Samarth Sridhara <117624746+TheSamurai1@users.noreply.github.com>
2024-04-28 12:44:31 -0700

    uncomment

--- CV-Pregrnancy.py
+++ CV-Pregrnancy.py
@@ -1,37 +1,23 @@
-import cv2 
+import cv2 as cv
 import numpy as np
 
-image = cv2.imread('Bob/1_2HC.png')
+img = cv.imread('Bob/1_2HC.png')
 
-blurred_img = cv2.GaussianBlur(image, (21, 21), 0)
-mask = np.zeros(image.shape, np.uint8)
+#image sharpening
+gaussian_blur = cv.GaussianBlur(img, (7,7), 2)
+sharpened = cv.addWeighted(img, 3.5, gaussian_blur, -2.5, 0)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[0]
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cv.imshow('Sharpened', sharpened)
 
-cv2.drawContours(mask, contours, -1, (255,255,255),5)
-# output = np.where(mask==np.array([255, 255, 255]), blurred_img, image)
+#cv.imshow("Pregnancy" , img)
 
-# kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-# im = cv.filter2D(img, -1, kernel)
-# cv.imshow("Imageeee", im)
-
-# #image sharpening
-# gaussian_blur = cv.GaussianBlur(img, (7,7), 2)
-# sharpened = cv.addWeighted(img, 3.5, gaussian_blur, -2.5, 0)
-
-# cv.imshow('Sharpened', sharpened)
-
-# #cv.imshow("Pregnancy" , img)
-
-# # gray = cv.cvtColor(sharpened, cv.COLOR_BGR2GRAY)
-# # cv.imshow('Gray', gray)
-# blur = cv.GaussianBlur(im, (7,7), cv.BORDER_DEFAULT)
+gray = cv.cvtColor(sharpened, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
 
 
-# cv.imshow('Blur', blur)
-# #works the best so far
+cv.imshow('Blur', blur)
+#works the best so far
 
 # # #Edge cascade
 # canny = cv.Canny(blur, 125, 175)
@@ -87,17 +73,17 @@ cv2.drawContours(mask, contours, -1, (255,255,255),5)
 # #works the best so far
 
 # # #Edge cascade
-# canny = cv.Canny(blur, 125, 175)
+canny = cv.Canny(blur, 125, 175)
 
-# ret, thresh = cv.threshold(gray, 125, 255, 0)
-# contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) 
-# cv.drawContours(img, contours, -1, (0,255,0), 3) 
-# area = cv.contourArea(contours[0]) 
-# scale_factor = 0.1
-# size = area * scale_factor ** 2
+ret, thresh = cv.threshold(gray, 125, 255, 0)
+contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) 
+cv.drawContours(img, contours, -1, (0,255,0), 3) 
+area = cv.contourArea(contours[0]) 
+scale_factor = 0.1
+size = area * scale_factor ** 2
 
-# print("size:", size)
-# cv.imshow("Pregnacy", canny)
+print("size:", size)
+cv.imshow("Pregnacy", canny)
 
 """" commented:
 laplacion, edge detection
@@ -113,4 +99,4 @@ sobel
  cv.imshow('Sobel Y', sobely)
 """
 
-cv2.waitKey(0)
+cv.waitKey(0)
