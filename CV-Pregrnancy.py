07ffa49d2ec81c28c477dc9ab4ae34aa0c24ae24
E.K. <fake.hack376@gmail.com>
2024-04-27 21:28:26 -0700

    Update CV-Pregrnancy.py

--- CV-Pregrnancy.py
+++ CV-Pregrnancy.py
@@ -24,17 +24,18 @@ size = area * scale_factor ** 2
 print("size:", size)
 cv.imshow("Pregnacy", canny)
 
+""""
+laplacion, edge detection
+ lap = cv.Laplacian(gray,cv.CV_64F)
+ lap = np.uint8(np.absolute(lap))
+ cv.imshow('Laplacian', lap)
 
-#laplacion, edge detection
-# lap = cv.Laplacian(gray,cv.CV_64F)
-# lap = np.uint8(np.absolute(lap))
-# cv.imshow('Laplacian', lap)
+sobel
+ sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+ sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
 
-#sobel
-# sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
-# sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
-
-# cv.imshow('Sobel X', sobelx)
-# cv.imshow('Sobel Y', sobely)
+ cv.imshow('Sobel X', sobelx)
+ cv.imshow('Sobel Y', sobely)
+"""
 
 cv.waitKey(0)
