77890f240f77d8112407b66ae5bf71f22c992a4d
Samarth Sridhara <117624746+TheSamurai1@users.noreply.github.com>
2024-04-27 20:11:27 -0700

    Cv stuff

---
+++ CV-Pregrnancy.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Bob/1_2HC.png')
+
+#cv.imshow("Pregnancy" , img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+#works the best so far
+
+# #Edge cascade
+canny = cv.Canny(blur, 125, 175)
+cv.imshow("Pregnacy", canny)
+
+
+#laplacion, edge detection
+# lap = cv.Laplacian(gray,cv.CV_64F)
+# lap = np.uint8(np.absolute(lap))
+# cv.imshow('Laplacian', lap)
+
+#sobel
+# sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+# sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+
+# cv.imshow('Sobel X', sobelx)
+# cv.imshow('Sobel Y', sobely)
+
+cv.waitKey(0)
